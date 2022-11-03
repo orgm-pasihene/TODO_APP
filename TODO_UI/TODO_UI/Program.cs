d290e0b2bbff672b727267345ba68fef1a6a7652
Prince Asihene <prince.asihene@orgmento.com>
2022-11-03 11:49:03 +0000

    fifth commit

--- TODO_UI/TODO_UI/Program.cs
+++ TODO_UI/TODO_UI/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Newtonsoft.Json.Serialization;
-using TODO_UI.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,7 +19,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 
 //JSON Serializer
@@ -54,3 +52,5 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
+
+await builder.Build().RunAsync();
